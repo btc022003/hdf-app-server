fa7643d3c97b4a285539bee68233203c608e91ae
Arivin <btc022003@gmail.com>
2022-03-08 16:50:57 +0800

    修改文章新增时的数据模型中分类信息异常

--- src/admin/articles/articles.service.ts
+++ src/admin/articles/articles.service.ts
@@ -5,6 +5,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class ArticlesService extends BaseService {
   constructor(private readonly prisma: PrismaService) {
-    super(prisma.article);
+    super(prisma.article, { category: true });
   }
 }
