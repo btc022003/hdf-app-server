970de29a68c1f75c10cd8b6036252849aa9741b8
Arivin <btc022003@gmail.com>
2022-06-23 15:15:30 +0800

    添加wangEditor富文本编辑器文件上传处理

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -2,9 +2,11 @@ import {
   BadRequestException,
   Controller,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import * as multer from 'multer';
@@ -40,6 +42,32 @@ export class CommonController {
       throw new BadRequestException('请选择需要上传的文件');
     } else {
       return '/uploads/' + files?.filename;
+    }
+  }
+
+  @Post('upload_wang_editor')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage,
+    }),
+  )
+  uploadFileWangRichEditor(
+    @UploadedFile()
+    files,
+    @Res({ passthrough: false }) // false表示手动发送，不使用全局管道拦截
+    response: Response,
+  ) {
+    if (files == undefined) {
+      throw new BadRequestException('请选择需要上传的文件');
+    } else {
+      return response.json({
+        errno: 0, // 注意：值是数字，不能是字符串
+        data: {
+          url: '/uploads/' + files?.filename, // 图片 src ，必须
+          // alt: '图片', // 图片描述文字，非必须
+          // href: 'zzz', // 图片的链接，非必须
+        },
+      });
     }
   }
 }
